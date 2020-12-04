7bb8aa29bb1699a93d7a9ff0d8e539927103d0be
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-12-03 18:47:36 -0800

    Moved over from elfin

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    type: 'blog',
+    singular: 'post',
+    template: {
+        contentPath: `content/blog/{{ slug }}.md`,
+        frontmatter: {
+            title: `{{ title }}`,
+            date: `{{ date }}`,
+        }
+    },
+    module: {
+        command: 'post <title>',
+        desc: 'Make a blog post',
+    }
+}
